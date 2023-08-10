8ea4014874b8a37ca207801e83867fff00478505
teien <ductien96er@gmail.com>
2023-08-10 09:39:37 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
+
 import AppRoutes from './routes/index';
-import Sidebar from './components/layouts/Sidebar';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<div className='App'>
-			<div className='d-flex align-start'>
-				<Sidebar />
-				<div className='w-100'>
-					{/* render page */}
-					<AppRoutes />
-				</div>
-			</div>
+			<Header />
+			<AppRoutes />
 		</div>
 	);
 }
