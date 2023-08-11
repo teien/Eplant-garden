c0e32174673b6b4ff5393d840c14e563a1296121
hunglevi93 <hung.levi93@gmail.com>
2023-08-11 20:28:18 +0700

    fix contact

--- src/pages/todo/contact.js
+++ src/pages/todo/contact.js
@@ -5,68 +5,29 @@ export default function Contact() {
             <section className="mb-4">
                 <h2 className="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
                 <p className="text-center w-responsive mx-auto mb-5">Do you have any questions? Please feel free to reach out to us directly. Our team will promptly respond within a few hours to assist you.</p>
-                <div className="row">
-                    <div className="col-md-9 mb-md-0 mb-5">
-                        <form id="contact-form" name="contact-form" action="mail.php" >
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="md-form mb-0">
-                                        <input type="text" id="name" name="name" className="form-control" />
-                                        <label for="name" className="">Your Name</label>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="md-form mb-0">
-                                        <input type="text" id="email" name="email" className="form-control" />
-                                        <label for="email" className="">Your Email</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="text" id="subject" name="subject" className="form-control" />
-                                        <label for="subject" className="">Subject</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-12">
-
-                                    <div className="md-form">
-                                        <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>
-                                        <label for="message">Your Message</label>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </form>
-                        <div className="text-center text-md-left">
-                            <button className="btn btn-primary" >Send</button>
-
+                <div className="row" >
+                    <div className="col-4">
+                        <div>
+                            <img style={{margin:"auto"}} className="d-block my-3" src="https://www.nike.com/assets/experience/dotcom-member/gethelpfe-webshell/_next/static/images/phone.png"/>
+                            <h5 style={{textAlign:"center"}}>PRODUCTS & ORDERS</h5>
+                            <h5 style={{textAlign:"center"}}>0382 020 858</h5>
+                            <h5 style={{textAlign:"center"}}>24 hours a day</h5>
+                            <h5 style={{textAlign:"center"}}>7 days a week</h5>
                         </div>
-                        <div className="status"></div>
                     </div>
-
-
-
-                    <div className="col-md-3 text-center">
-                        <ul className="list-unstyled mb-0">
-                            <li><i className="fas fa-map-marker-alt fa-2x"></i>
-                                <p>Thủy Lợi University, Việt Nam</p>
-                            </li>
-
-                            <li><i className="fas fa-phone mt-4 fa-2x"></i>
-                                <p> 0382 020 858</p>
-                            </li>
-
-                            <li><i className="fas fa-envelope mt-4 fa-2x"></i>
-                                <p>Homies@aptech.vn</p>
-                            </li>
-                        </ul>
+                    <div className="col-4">
+                    <div>
+                            <img style={{margin:"auto"}} className="d-block my-3" src="https://www.nike.com/assets/experience/dotcom-member/gethelpfe-webshell/_next/static/images/chat.png"/>
+                            <h5 style={{textAlign:"center"}}>PRODUCTS & ORDERS</h5>
+                            <h5 style={{textAlign:"center"}}>Homies@aptech.vn</h5>
+                            <h5 style={{textAlign:"center"}}>24 hours a day</h5>
+                            <h5 style={{textAlign:"center"}}>7 days a week</h5>
+                        </div>
                     </div>
-
-
+                    <div className="col-4">
+                    <img style={{margin:"auto"}} className="d-block my-3" src="https://www.nike.com/assets/experience/dotcom-member/gethelpfe-webshell/_next/static/images/store.png"/>
+                            <h5 style={{textAlign:"center"}}>Thủy Lợi University, Việt Nam</h5>
+                    </div>
                 </div>
 
             </section>
