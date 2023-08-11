aba31ff1f26e7565d68b1cf3704c0091bc809032
teien <ductien96er@gmail.com>
2023-08-11 13:01:57 +0700

    addcontact

--- src/pages/todo/contact.js
+++ src/pages/todo/contact.js
@@ -1,24 +1,76 @@
-import '../../assets/styles/contact.css';
+
 export default function Contact() {
-    return ( 
-    <div className="container-fluid d-flex">
-            <div className="container">
-                <div className='row d-flex'>
-                    <div class="footer-item col-6">
-                        <div className="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    return (
+        <div className="border border-5 m-auto mt-5" style={{ width: '1200px' }}>
+            <section className="mb-4">
+                <h2 className="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+                <p className="text-center w-responsive mx-auto mb-5">Do you have any questions? Please feel free to reach out to us directly. Our team will promptly respond within a few hours to assist you.</p>
+                <div className="row">
+                    <div className="col-md-9 mb-md-0 mb-5">
+                        <form id="contact-form" name="contact-form" action="mail.php" >
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="md-form mb-0">
+                                        <input type="text" id="name" name="name" className="form-control" />
+                                        <label for="name" className="">Your Name</label>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="md-form mb-0">
+                                        <input type="text" id="email" name="email" className="form-control" />
+                                        <label for="email" className="">Your Email</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="md-form mb-0">
+                                        <input type="text" id="subject" name="subject" className="form-control" />
+                                        <label for="subject" className="">Subject</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+
+                                    <div className="md-form">
+                                        <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>
+                                        <label for="message">Your Message</label>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                        <div className="text-center text-md-left">
+                            <button className="btn btn-primary" >Send</button>
+
                         </div>
-                        <div className="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Feed Back</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
+                        <div className="status"></div>
                     </div>
-                    <div className="footer-item col-6 ms-4">
-                            <img src='https://i.pinimg.com/236x/df/f9/e0/dff9e0e92f27f8946fa21274cfdb6691.jpg'/>
+
+
+
+                    <div className="col-md-3 text-center">
+                        <ul className="list-unstyled mb-0">
+                            <li><i className="fas fa-map-marker-alt fa-2x"></i>
+                                <p>Thủy Lợi University, Việt Nam</p>
+                            </li>
+
+                            <li><i className="fas fa-phone mt-4 fa-2x"></i>
+                                <p> 0382 020 858</p>
+                            </li>
+
+                            <li><i className="fas fa-envelope mt-4 fa-2x"></i>
+                                <p>Homies@aptech.vn</p>
+                            </li>
+                        </ul>
                     </div>
-                </div>  
+
+
+                </div>
+
+            </section>
+
         </div>
-    </div>
     )
 }
