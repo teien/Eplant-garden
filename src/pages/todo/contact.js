03eddc38603d62ad46d7e8a99b0242786acc750d
SonDo0512 <dosonbibong@gmail.com>
2023-08-11 19:06:16 +0700

    code

--- src/pages/todo/contact.js
+++ src/pages/todo/contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     return ( 
     <div className="container-fluid d-flex">
             <div className="container">
-                <div className='row d-flex'>
+                <div className='row'>
                     <div class="footer-item col-6">
                         <div className="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Email address</label>
@@ -14,8 +14,8 @@ export default function Contact() {
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
                     </div>
-                    <div className="footer-item col-6 ms-4">
-                            <img src='https://i.pinimg.com/236x/df/f9/e0/dff9e0e92f27f8946fa21274cfdb6691.jpg'/>
+                    <div className="footer-item col-6 ">
+                        <img src="https://i.pinimg.com/564x/be/50/59/be50595e4a73161ee35c198023fdbd1d.jpg" class="img-fluid" alt="..."/> 
                     </div>
                 </div>  
         </div>
