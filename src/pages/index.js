93605f041ef276406af6d232cd201f3d9f2e8e13
teien <ductien96er@gmail.com>
2023-08-11 21:15:07 +0700

    setting deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function IndexPage() {
 						<img src="https://khothietke.net/wp-content/uploads/2021/04/PNGKhovector.net-02186.png" className="img-fluid rounded-start" alt="..." />
 					</div>
 					<div className="col-md-8">
-						<div className="card-body mt-5 ms-3" style={{ width: '500px' }}>
+						<div className="card-body mt-5 ms-3 " style={{ width: '500px' }}>
 							<h1 className="card-title fs-1 fw-bold">We Love Helping To Your Decorate Home Plants</h1>
 							<p className="card-text fs-4">- Garden care is the process of maintaining and nurturning a garden to ensure its health, beauty, and productivity.</p>
 							<h1 className="card-title fs-1 fw-bold">Our service</h1>
