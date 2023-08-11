aa69fd75cdcd2d4ac14ceed5dc2682d0a226622c
hunglevi93 <hung.levi93@gmail.com>
2023-08-11 19:25:56 +0700

    dasda

--- src/pages/buytree.js
+++ src/pages/buytree.js
@@ -56,7 +56,7 @@ export default function BuyTree() {
                 </tbody>
             </table>
             <div className="row" style={{
-                width: "85%",
+                width: "100%",
                 height: 80,
                 backgroundColor: "#eeeee4",
                 position: "fixed",
