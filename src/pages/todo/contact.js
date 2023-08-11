1e1021b11c45070e2c635bcdef9b1773a747fc15
SonDo0512 <dosonbibong@gmail.com>
2023-08-11 10:10:55 +0700

    code-son

--- src/pages/todo/contact.js
+++ src/pages/todo/contact.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 export default function Contact() {
     return ( 
     <div className="container-fluid d-flex">
-            <div className="container d-flex">
+            <div className="container">
                 <div className='row'>
                     <div class="footer-item col-6">
                         <div class="mb-3">
@@ -14,7 +14,7 @@ export default function Contact() {
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
                     </div>
-                    <div class="footer-item col-6">
+                    <div className="footer-item col-6 ms-4">
                             <img src='https://i.pinimg.com/236x/df/f9/e0/dff9e0e92f27f8946fa21274cfdb6691.jpg'/>
                     </div>
                 </div>  
